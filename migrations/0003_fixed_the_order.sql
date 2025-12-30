6221f75aeee3f16f42b3632219dd1b67381fc09c
Marc-Etienne Dartus <marc-etienne.dartus@sanofi.com>
2025-12-31 00:34:01 +0100

    fix: correct enum value in migration from 'redis' to 'hot'
    - change storage_location default from invalid 'redis' to valid 'hot'
    - fixes build error: invalid input value for enum cache_location
    - enum only allows: hot, cold, none

--- migrations/0003_fixed_the_order.sql
+++ migrations/0003_fixed_the_order.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "rendered_pages" ADD COLUMN "api_key_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "rendered_pages" ADD COLUMN "storage_location" "cache_location" DEFAULT 'redis' NOT NULL;--> statement-breakpoint
+ALTER TABLE "rendered_pages" ADD COLUMN "storage_location" "cache_location" DEFAULT 'hot' NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "rendered_pages" ADD CONSTRAINT "rendered_pages_api_key_id_api_keys_id_fk" FOREIGN KEY ("api_key_id") REFERENCES "public"."api_keys"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
